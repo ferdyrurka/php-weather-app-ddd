2b5feafad66f12309ebae2d549d9eb4fd579ca50
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-03-15 22:12:38 +0100

    CreateWeatherFactory

---
+++ src/Application/Factory/CreateWeatherFactory.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Application\Factory;
+
+use App\Domain\Entity\Weather;
+
+/**
+ * Class CreateWeatherFactory
+ * @package App\Application\Factory
+ */
+class CreateWeatherFactory
+{
+    /**
+     * @var array
+     */
+    private $data;
+
+    /**
+     * CreateWeatherFactory constructor.
+     * @param array $data
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @return Weather
+     */
+    public function getWeather(): Weather
+    {
+        $weather = new Weather();
+        $weather->setCityName($this->data['name']);
+        $weather->setDataJson(\json_encode($this->data));
+    }
+}
