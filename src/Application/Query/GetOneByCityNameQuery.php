08519bde87963d42fb584d7bbab2d84657908616
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-03-15 22:55:32 +0100

    GetOneByCityNameQuery

---
+++ src/Application/Query/GetOneByCityNameQuery.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Application\Query;
+
+use Ferdyrurka\CommandBus\Query\QueryInterface;
+
+/**
+ * Class GetOneByCityNameQuery
+ * @package App\Application\Query
+ */
+class GetOneByCityNameQuery implements QueryInterface
+{
+    /**
+     * @var string
+     */
+    private $cityName;
+
+    /**
+     * GetOneByCityNameQuery constructor.
+     * @param string $cityName
+     */
+    public function __construct(string $cityName)
+    {
+        $this->cityName = $cityName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCityName(): string
+    {
+        return $this->cityName;
+    }
+}
