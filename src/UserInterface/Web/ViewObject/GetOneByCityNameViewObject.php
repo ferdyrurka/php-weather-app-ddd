1818d69604a61b55559af5fe16ef95df64780d78
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-03-15 23:04:03 +0100

    GetOneByCityNameViewObject

---
+++ src/UserInterface/Web/ViewObject/GetOneByCityNameViewObject.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\UserInterface\Web\ViewObject;
+
+use Ferdyrurka\CommandBus\Query\ViewObject\ViewObjectInterface;
+
+/**
+ * Class GetOneByCityNameViewObject
+ * @package App\UserInterface\Web\ViewObject
+ */
+class GetOneByCityNameViewObject implements ViewObjectInterface
+{
+    /**
+     * @var array
+     */
+    private $data;
+
+    /**
+     * GetOneByCityNameViewObject constructor.
+     * @param array $data
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @return array
+     */
+    public function getData(): array
+    {
+        return $this->data;
+    }
+}
