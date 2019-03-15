16e59da9cd747af3feda8cf4e9c8b56c8485b053
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-03-15 19:40:00 +0100

    WeatherAppException

---
+++ src/Domain/Exception/WeatherAppException.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Domain\Exception;
+
+use \Exception;
+
+/**
+ * Class WheatherAppException
+ * @package App\Domain\Exception
+ */
+class WeatherAppException extends Exception
+{
+}
