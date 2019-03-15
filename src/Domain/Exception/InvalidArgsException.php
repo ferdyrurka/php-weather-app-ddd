018305c76974531b78a3fbb922e0433a22261174
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-03-15 23:39:11 +0100

    InvalidArgsException

---
+++ src/Domain/Exception/InvalidArgsException.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Domain\Exception;
+
+/**
+ * Class InvalidArgsException
+ * @package App\Domain\Exception
+ */
+class InvalidArgsException extends WeatherAppException
+{
+}
