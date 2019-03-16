ccb2789c5c28404c88e8c6bd0f82050c3b2798d2
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-03-16 14:08:28 +0100

    ServerOWMException

---
+++ src/Domain/Exception/ServerOWMException.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Domain\Exception;
+
+/**
+ * Class ServerOWMException
+ * @package App\Domain\Exception
+ */
+class ServerOWMException extends WeatherAppException
+{
+}
