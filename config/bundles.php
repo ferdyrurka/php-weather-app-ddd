61348415ad8161500df01322db727046afc9cac3
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-03-28 12:32:54 +0100

    bundles

--- config/bundles.php
+++ config/bundles.php
@@ -11,5 +11,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    \Ferdyrurka\CommandBus\CommandBusSymfonyBundle::class => ['all' => true],
+    Ferdyrurka\CommandBus\CommandBusSymfonyBundle::class => ['all' => true],
 ];
